231792c013e993876de81fe3d7282c67c2cd3620
Nikolay <deathmetal9812@gmail.com>
2026-07-07 18:27:00 +0200

    feat: add crypto shim for webcrypto support in metro configuration

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,6 +1,12 @@
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  crypto: path.resolve(__dirname, "lib/node-crypto-shim.js"),
+};
 
 module.exports = withNativeWind(config, { input: "./global.css" });
